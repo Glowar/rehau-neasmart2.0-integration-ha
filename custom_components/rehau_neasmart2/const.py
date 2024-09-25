a8e9ac7060bb634e74442740edd4d3d716405d41
Alexander <glowar@glowar.ru>
2024-09-26 00:34:24 +0300

    Update const.py

--- custom_components/rehau_neasmart2/const.py
+++ custom_components/rehau_neasmart2/const.py
@@ -10,19 +10,19 @@ PRESENCE_STATES = {
     False: "Not Present"
 }
 PRESET_STATES_MAPPING = {
-    "Normal": 1,
-    "Reduced": 2,
-    "Standby": 3,
-    "Time Program": 4,
-    "Party": 5,
-    "Absence": 6
+    "Обычный": 1,
+    "Экономичный": 2,
+    "Режим ожидания": 3,
+    "Авто": 4,
+    "Вечерина": 5,
+    "Отпуск": 6
 }
 PRESET_STATES_MAPPING_REVERSE = {v: k for k, v in PRESET_STATES_MAPPING.items()}
 PRESET_CLIMATE_MODES_MAPPING = {
-    "Auto": 1,
-    "Heating": 2,
-    "Cooling": 3,
-    "Forced Heating": 4,
-    "Forced Cooling": 5
+    "Авто": 1,
+    "Отопление": 2,
+    "Охлаждение": 3,
+    "Ручной режим отопленияв": 4,
+    "Ручной режим охлаждения": 5
 }
 PRESET_CLIMATE_MODES_MAPPING_REVERSE = {v: k for k, v in PRESET_CLIMATE_MODES_MAPPING.items()}
